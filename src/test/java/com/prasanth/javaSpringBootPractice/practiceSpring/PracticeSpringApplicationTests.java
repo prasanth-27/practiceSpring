42b6c8f7921f89b8a9b391a7a70fed32cff0f051
prasanth-27 <yvsaiprasanth@gamil.com>
2025-03-29 20:53:53 +0530

    Base spring setup

---
+++ src/test/java/com/prasanth/javaSpringBootPractice/practiceSpring/PracticeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasanth.javaSpringBootPractice.practiceSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
